f6a8ef2eebc14726a438c6c1f0c0a043a14c55fe
Hannah Riedman <hannah.riedman1@marist.edu>
2018-04-01 21:24:49 -0400

    Created ReadFile class.
    This is important in properly reading the file that will be needed.

--- grephy.java
+++ grephy.java
@@ -9,20 +9,91 @@
  */
 
 import java.io.*;
+
 //import java.util.regex;
 
 public class Grephy {
 
+  public static class ReadFile {
+    private String file;
+
+    public ReadFile (String file_path) {
+      file = file_path;
+    }
+
+    public String[] OpenFile() throws IOException {
+      FileReader fr = new FileReader(file);
+      BufferedReader input = new BufferedReader(fr);
+      int numberofLines = readLines();
+      String[] data = new String[numberofLines];
+
+      for (int i = 0; i < numberofLines; i++) {
+        data[i] = input.readLine();
+        System.out.println(data[i]);
+      }
+      input.close();
+      return data;
+    }
+
+    int readLines() throws IOException {
+      FileReader fileToRead = new FileReader(file);
+      BufferedReader input = new BufferedReader(fileToRead);
+
+      String line;
+      int numberOfLines = 0;
+
+      while ((line = input.readLine())!= null) {
+        numberOfLines++;
+      }
+      input.close();
+      return numberOfLines;
+    }
+
+  }
+ /**
+  * TODO clean up code (especially ReadFile)
+  * TODO add in parameters to check for all parameters
+  * TODO add in something to account for directory?
+  * TODO ^ if so this must account for System (Windows or Mac)
+  * TODO create logger/debug for system.out prints
+  * TODO start creating files for NFA, DFA, etc 
+  *
+ */
 	public static void main(String[] args) throws IOException {
-    BufferedReader input =  // standard input
+    BufferedReader in =  // standard input
       new BufferedReader(new InputStreamReader(System.in));
 
+
     // Read and echo lines until EOF.
-    System.out.println("Regex:");
-    String regex1 = input.readLine();
+    System.out.println("Welcome to Grephy!");
+    String s = in.readLine();
+    while (s!=null) {
+      // exit condition
+      if (s.toLowerCase().contains("exit") || s.toLowerCase().contains("quit") || s.toLowerCase().contains("end")) {
+          break;
+      } else {
+        String file_name = s;
+        try {
+          ReadFile file = new ReadFile(file_name);
+          String[] lines = file.OpenFile();
+        } catch (IOException e) {
+          System.out.println(e.getMessage());
+          System.out.println("hello");
+        }
+      }
+      System.out.println("Welcome to Grephy!");
+      s = in.readLine();
+    }
+    System.out.println("hello");
+    //String file_name = "/Users/hannahriedman/Documents/workspace/Grephy1/src/test1.txt";
+
+
+    // Read and echo lines until EOF.
+  //  System.out.println("Regex:");
+    //String regex1 = input.readLine();
     //while (regex!=null) {
-      System.out.println(regex1);
-      regex1 = input.readLine();
+    //  System.out.println(regex1);
+    //  regex1 = input.readLine();
     //}
 
 	}
