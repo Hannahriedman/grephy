78158fb117eeb0c6cd12f9c34190e36cbd44b26d
Hannah Riedman <hannah.riedman1@marist.edu>
2018-04-30 23:57:28 -0400

    Added a WriteFile class to deal with dot files and NFA and DFA implementation

--- grephy.java
+++ grephy.java
@@ -12,90 +12,142 @@ import java.io.*;
 
 //import java.util.regex;
 
-public class Grephy {
+public class grephy {
 
-  public static class ReadFile {
-    private String file;
+    public static class WriteFile {
+        private final String outfile;
+        static StringBuilder dotfile = new StringBuilder();
 
-    public ReadFile (String file_path) {
-      file = file_path;
-    }
-
-    public String[] OpenFile() throws IOException {
-      FileReader fr = new FileReader(file);
-      BufferedReader input = new BufferedReader(fr);
-      int numberofLines = readLines();
-      String[] data = new String[numberofLines];
-
-      for (int i = 0; i < numberofLines; i++) {
-        data[i] = input.readLine();
-        System.out.println(data[i]);
-      }
-      input.close();
-      return data;
-    }
-
-    int readLines() throws IOException {
-      FileReader fileToRead = new FileReader(file);
-      BufferedReader input = new BufferedReader(fileToRead);
-
-      String line;
-      int numberOfLines = 0;
-
-      while ((line = input.readLine())!= null) {
-        numberOfLines++;
-      }
-      input.close();
-      return numberOfLines;
-    }
-
-  }
- /**
-  * TODO clean up code (especially ReadFile)
-  * TODO add in parameters to check for all parameters
-  * TODO add in something to account for directory?
-  * TODO ^ if so this must account for System (Windows or Mac)
-  * TODO create logger/debug for system.out prints
-  * TODO start creating files for NFA, DFA, etc 
-  *
- */
-	public static void main(String[] args) throws IOException {
-    BufferedReader in =  // standard input
-      new BufferedReader(new InputStreamReader(System.in));
-
-
-    // Read and echo lines until EOF.
-    System.out.println("Welcome to Grephy!");
-    String s = in.readLine();
-    while (s!=null) {
-      // exit condition
-      if (s.toLowerCase().contains("exit") || s.toLowerCase().contains("quit") || s.toLowerCase().contains("end")) {
-          break;
-      } else {
-        String file_name = s;
-        try {
-          ReadFile file = new ReadFile(file_name);
-          String[] lines = file.OpenFile();
-        } catch (IOException e) {
-          System.out.println(e.getMessage());
-          System.out.println("hello");
+        public WriteFile(String file_path) {
+            outfile = file_path;
         }
-      }
-      System.out.println("Welcome to Grephy!");
-      s = in.readLine();
+
+        public NFA ProcessNFA (String[] regex, int i) {
+            //NFA nfa1 = new NFA();
+            RegexParser parser = new RegexParser(regex[i]);
+            NFA nfa1 = parser.regex();
+            System.out.println(nfa1.toDOT());
+            String text = nfa1.toDOT();
+            dotfile.append(text);
+            return nfa1;
+        }
+
+        public void ProcessDFA (NFA nfa) {
+            //DFA dfa1 = new DFA();
+            //dfa1.streamIn(nfa);
+
+            //dotfile.append(text);
+        }
+
+        public void OutputFile() {
+            try {
+                File file = new File(outfile);
+                FileWriter fileWriter = new FileWriter(file);
+                fileWriter.write(dotfile.toString());
+                fileWriter.flush();
+                fileWriter.close();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+
+        }
     }
-    System.out.println("hello");
-    //String file_name = "/Users/hannahriedman/Documents/workspace/Grephy1/src/test1.txt";
 
 
-    // Read and echo lines until EOF.
-  //  System.out.println("Regex:");
-    //String regex1 = input.readLine();
-    //while (regex!=null) {
-    //  System.out.println(regex1);
-    //  regex1 = input.readLine();
-    //}
+    public static class ReadFile {
+        private String file;
 
-	}
+        public ReadFile(String file_path) {
+            file = file_path;
+        }
 
+        public String[] OpenFile() throws IOException {
+            FileReader fr = new FileReader(file);
+            BufferedReader input = new BufferedReader(fr);
+            int numberofLines = readLines();
+            String[] data = new String[numberofLines];
+
+            for (int i = 0; i < numberofLines; i++) {
+                data[i] = input.readLine();
+                System.out.println(data[i]);
+            }
+            input.close();
+            return data;
+        }
+
+        int readLines() throws IOException {
+            FileReader fileToRead = new FileReader(file);
+            BufferedReader input = new BufferedReader(fileToRead);
+
+            int numberOfLines = 0;
+
+            while (input.readLine() != null) {
+                numberOfLines++;
+            }
+            input.close();
+            return numberOfLines;
+        }
+
+    }
+
+    /**
+     * TODO create logger/debug for system.out prints
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader in =  // standard input
+                new BufferedReader(new InputStreamReader(System.in));
+
+        //Logger logger = Logger.getLogger("grephy");
+        //logger.setLevel(Level.DEBUG);
+        // Read and echo lines until EOF.
+        System.out.println("Welcome to Grephy! Enter in a regex file (with path)");
+        System.out.println("Followed by a path and name for your NFA and DFA files");
+        System.out.println("To see a proper file format, type 'example'");
+        String s = in.readLine();
+        while (s != null) {
+            // exit condition
+            if (s.toLowerCase().contains("exit") || s.toLowerCase().contains("quit")
+                    || s.toLowerCase().contains("end")) {
+                break;
+            } else if (s.toLowerCase().contains("example")){
+                System.out.println("/Users/hannahriedman/Documents/workspace/Grephy1/src/test1.txt " +
+                        "/Users/hannahriedman/Documents/workspace/Grephy1/src/dfa.dot " +
+                        "/Users/hannahriedman/Documents/workspace/Grephy1/src/dfa1.dot\n");
+            } else {
+                String[] line = s.split(" ");
+                String regex1 = line[0];
+                String NFA_file = line[1];
+                String DFA_file = line[2];
+                try {
+                    ReadFile file = new ReadFile(regex1);
+                    String[] lines = file.OpenFile();
+                    WriteFile outputNFA = new WriteFile(NFA_file);
+                    WriteFile outputDFA = new WriteFile(DFA_file);
+                    System.out.println(lines.length);
+                    for (int i=0; i < lines.length;i++) {
+                        NFA nfa = outputNFA.ProcessNFA(lines,i);
+                        outputNFA.ProcessDFA(nfa);
+                    }
+                    outputNFA.OutputFile();
+                    outputDFA.OutputFile();
+
+                    System.out.println("NFA file at: " + NFA_file);
+                    System.out.println("DFA file at: " + DFA_file);
+                    System.out.println("Thanks for using Grephy!");
+                    System.out.println("---------------------------");
+                    System.out.println("We are currently working to have a visual representation available for dot files,");
+                    System.out.println("Until then, visit http://www.webgraphviz.com/ to view your files. :)");
+                    break;
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                    System.out.println("Error Reading File");
+                }
+            }
+            System.out.println("Enter in file name (with path)");
+            s = in.readLine();
+        }
+
+    }
 }
+
+
